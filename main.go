cdc320cf9500a848d89767c61de2f1f3aa1b33bc
Gurpreet Singh <gurpreetbassan@gmail.com>
2021-03-15 16:45:34 +0530

    env vars

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "os"
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test CI Webserver 3")
+    config_map_update := os.Getenv("CONFIG_VERSION")
+    image_version := os.Getenv("IMAGE_VERSION")
+    fmt.Fprintf(w, "Test CI Webserver\nconfig version: %s\nimage version: %s", config_map_update, image_version)
 }
 
 func main() {
